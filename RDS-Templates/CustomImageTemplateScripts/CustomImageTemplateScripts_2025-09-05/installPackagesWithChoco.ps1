5dcbe89cb7f39f14a2e58fc46f56dd61b49573ae
Joseph Karns <jkarns87@gmail.com>
2025-11-06 10:52:00 -0500

    Fixing the Try/Catch/Finally Block

--- RDS-Templates/CustomImageTemplateScripts/CustomImageTemplateScripts_2025-09-05/installPackagesWithChoco.ps1
+++ RDS-Templates/CustomImageTemplateScripts/CustomImageTemplateScripts_2025-09-05/installPackagesWithChoco.ps1
@@ -24,38 +24,14 @@ function InstallPackagesWithChoco($packageArray) {
         foreach ($Package in $PackageList) {
             try {
                 Write-host "AVD Customization: Install Packages with Chocolatey - Installing $Package"
-                $maxAttempts = 5
-                $attempt = 0
-                $success = $false
-
-                while (-not $success -and $attempt -lt $maxAttempts) {
-                    $attempt++
-                    Write-Host "Attempting to Install $Package with Chocolatey (Attempt $attempt of $maxAttempts)..."
-
-                    try {
-                        Start-Process choco -ArgumentList "install $Package --ignoredetectedreboot --yes --use-package-exit-codes --ignore-checksums --install-args '/allusers'" -PassThru -Wait
-                        if (choco list --lo -r -e $Package) {
-                            Write-Host "AVD Customization: Install Packages with Chocolatey - $Package is installed."
-                            $success = $true
-                        } else {
-                            throw
-                        }
-                    }
-                    catch {
-                        Write-Host "Error: $($_.Exception.Message)"
-                        if ($attempt -lt $maxAttempts) {
-                            Write-Host "AVD Customization: Install Packages with Chocolatey - $Package is not installed."
-                            Write-Host "Retrying in 10 seconds..."
-                            Start-Sleep -Seconds 10
-                        } else {
-                            Write-Host "AVD Customization: Install Packages with Chocolatey - $Package is not installed."
-                            Write-Host "Maximum attempts reached. Operation failed."
-                        }
-                    }
-                }
+                Start-Process choco -ArgumentList "install $Package --ignoredetectedreboot --yes --use-package-exit-codes --ignore-checksums --debug --verbose --install-args '/allusers'" -ErrorAction Stop -Wait
             }
             catch {
-                Write-Host "*** AVD CUSTOMIZER PHASE ***  Install Packages with Chocolatey  - Exception occured installing $Package  *** : [$($_.Exception.Message)]"
+                Write-Host "*** AVD CUSTOMIZER PHASE : Install Packages with Chocolatey  - Exception occured installing $Package  *** : [$($_.Exception.Message)]"
+            }
+            finally {
+                Start-Process choco list --lo -r -e $Package -Wait -PassThru
+                Write-Host "AVD Customization: Install Packages with Chocolatey - $Package is installed."
             }
         }
     }
